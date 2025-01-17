c01a872b668c5d3c25cdb2d808569d2d55d4be97
zidan <zidanlangoet10987@gmail.com>
2025-01-17 07:16:01 +0700

    masuk

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-const angka = 10
+console.log("Hai Kawanh");
+setTimeout(() => {
+  console.log("P");
+}, 3000);
+
+const promise = new Promise((resolve, reject) => {});
+
+const zidan = "Muhamad Zidan";
+
+if ((zidan = "Muhamad Zidan")) {
+  console.log("Kamu Benar");
+} else {
+  console.log("Kamu Salah");
+}
+
+console.log("Fitur A");
+console.log("Belajar Adalah Hal Yang Menyenangkan");
+function makan() {
+  console.log("Bagaimana adalah Sebuah Pertanyaan");
+  const zidan = Math.random();
+  if (zidan === 0.2) {
+    console.log(
+      'Benar, Sebuah Pertanyaan yang bisa dijawab dengan "Siap" adalah "Apakah Anda Siap?"'
+    );
+  } else {
+    console.log(
+      'Salah, Sebuah Pertanyaan yang bisa dijawab dengan "Siap" adalah "Apakah Anda Siap?"'
+    );
+  }
+} //
+
+for (i = 10; i < 10; i ++) {
+  console.log(i)
+}
